161c11d45c332275c0c4a61159b8e0ac6a70adb7
it <yeongbin0909@naver.com>
2025-01-12 13:40:43 +0900

    문자열 관련 모듈 수정

--- commonModule/text_tasker.py
+++ commonModule/text_tasker.py
@@ -1,33 +1,46 @@
 import hgtk
+import hgtk.exception
 
-def replaceJae(text):
+
+def isCompleteHangul(char: str) -> bool:
+    if len(char) != 1: raise hgtk.exception.NotLetterException
+
+    if hgtk.checker.is_hangul(char):
+        chosung, jungsung, _ = hgtk.letter.decompose(char)
+        if chosung == '' or jungsung == '': return False
+        else: return True
+
+    else: hgtk.exception.NotHangulException
+
+
+def replaceMoe(text, oldMoe: str, newMoe: str):
+    # ㅐㅒㅔㅖ 전부 통일하는거
+
     result = ''
+
     for char in text:
         if hgtk.checker.is_hangul(char):
-            try:
-                chosung, jungsung, jongsung = hgtk.letter.decompose(char)
 
-                # print(f'"{chosung}" "{jungsung}" "{jongsung}"')
-                
-                # 글자가 ㄱ ㅋㅋ ㅇㅇ 과 같이 자음만 있으면
-                if jungsung == '' and jongsung == '':
-                    result += char
-                    continue
-                # 글자가 ㅏ ㅣ ㅙ ㅒ 같이 모음만 있으면
-                elif chosung == '' and jongsung == '':
-                    result += char
-                    continue
+            chosung, jungsung, jongsung = hgtk.letter.decompose(char)
 
-                # ㅔ -> ㅐ, ㅖ -> ㅒ
-                if jungsung == 'ㅔ':
-                    jungsung = 'ㅐ'
-                elif jungsung == 'ㅖ':
-                    jungsung = 'ㅐ'
-                elif jungsung == 'ㅒ':
-                    jungsung = 'ㅐ'
-
+            # 완전한 한글 (애 아 엥 욍 등등) 일 경우
+            if chosung != '' and jungsung != '': # 종성 여부는 상관 없음
+                if jungsung in oldMoe: jungsung = newMoe
                 result += hgtk.letter.compose(chosung, jungsung, jongsung)
-            except: result += char
+
+            # 자음만 있을경우 (ㅁ ㄷ ㅃ ㄲ ㅋ)
+            elif chosung != '' and jungsung == '':
+                result += char
+
+            # 모음만 있을경우 ㅔㅐㅏㅓㅣ
+            elif chosung == '' and jungsung != '':
+                if jungsung in oldMoe: jungsung = newMoe
+                result += jungsung
+
+            # 종성만 있을경우 (ㄿ 같은 일부 문자는 종성에만 들어갈수 있음)
+            elif chosung == '' and jungsung == '' and jongsung != '':
+                result += char
+
         else:
             result += char  # 한글이 아니면 그대로 추가
     
@@ -35,30 +48,105 @@ def replaceJae(text):
 
 def replaceDoubleJae(text):
     # 쌍자음을 단자음으로 매핑
-    double_consonants = {
-        'ㄲ': 'ㄱ', 'ㄸ': 'ㄷ', 'ㅃ': 'ㅂ', 'ㅆ': 'ㅅ', 'ㅉ': 'ㅈ'
+    doubleJaeTable = {
+        'ㄲ': 'ㄱ', 
+        'ㄳ': 'ㄱ',
+
+        'ㄵ': 'ㄴ',
+        'ㄶ': 'ㄴ',
+
+        'ㄸ': 'ㄷ', 
+
+        'ㄺ': 'ㄹ',
+        'ㄻ': 'ㄹ',
+        'ㄼ': 'ㄹ',
+        'ㄽ': 'ㄹ',
+        'ㄾ': 'ㄹ',
+        'ㄿ': 'ㄹ',
+        'ㅀ': 'ㄹ',
+
+
+        'ㅃ': 'ㅂ', 
+        'ㅄ': 'ㅂ',
+
+        'ㅆ': 'ㅅ', 
+        'ㅉ': 'ㅈ'
     }
     
     result = ''
+
     for char in text:
         if hgtk.checker.is_hangul(char):
-            try:
-                chosung, jungsung, jongsung = hgtk.letter.decompose(char)
 
-                if jungsung == '' and jongsung == '':
-                    result += char
-                    continue
-                
+            chosung, jungsung, jongsung = hgtk.letter.decompose(char)
+
+            # 완전한 한글 (애 아 엥 욍 등등) 일 경우
+            if chosung != '' and jungsung != '': # 종성 여부는 상관 없음
                 # 쌍자음인 경우 단자음으로 변경
-                if chosung in double_consonants:
-                    chosung = double_consonants[chosung]
-                
+                if chosung in doubleJaeTable: chosung = doubleJaeTable[chosung]
+                if jongsung in doubleJaeTable: jongsung = doubleJaeTable[jongsung]
                 result += hgtk.letter.compose(chosung, jungsung, jongsung)
-            except: result += char
+            
+            # 자음만 있을경우 (ㅁ ㄷ ㅃ ㄲ ㅋ)
+            elif chosung != '' and jungsung == '':
+                if chosung in doubleJaeTable: chosung = doubleJaeTable[chosung]
+                result += chosung
+
+            # 모음만 있을경우 ㅔㅐㅏㅓㅣ
+            elif chosung == '' and jungsung != '':
+                result += char
+
+            # 종성만 있을경우 (ㄿ 같은 일부 문자는 종성에만 들어갈수 있음)
+            elif chosung == '' and jungsung == '' and jongsung != '':
+                if jongsung in doubleJaeTable: jongsung = doubleJaeTable[jongsung]
+                result += jongsung
+            
         else:
             result += char  # 한글이 아니면 그대로 추가
     
     return result
+
+def replaceDoubleMoe(text):
+    # 쌍모음을 단모음으로
+    doubleMoeTable = {
+        'ㅒ': 'ㅐ',
+        'ㅖ': 'ㅔ',
+        'ㅛ': 'ㅗ',
+        'ㅠ': 'ㅜ'
+    }
+    
+    result = ''
+
+    for char in text:
+        if hgtk.checker.is_hangul(char):
+
+            chosung, jungsung, jongsung = hgtk.letter.decompose(char)
+
+            # 완전한 한글 (애 아 엥 욍 등등) 일 경우
+            if chosung != '' and jungsung != '': # 종성 여부는 상관 없음
+                # 쌍모음인 경우 단모음 으로
+                if jungsung in doubleMoeTable: jungsung = doubleMoeTable[jungsung]
+                result += hgtk.letter.compose(chosung, jungsung, jongsung)
+            
+            # 자음만 있을경우 (ㅁ ㄷ ㅃ ㄲ ㅋ)
+            elif chosung == '' and jungsung != '':
+                result += char
+
+            # 모음만 있을경우 (ㅁ ㄷ ㅃ ㄲ ㅋ)
+            elif chosung != '' and jungsung == '':
+                # 쌍모음인 경우 단모음 으로
+                if jungsung in doubleMoeTable: jungsung = doubleMoeTable[jungsung]
+                result += hgtk.letter.compose(chosung, jungsung, jongsung)
+
+            # 종성만 있을경우 (일부 자음은 종성에만 들어갈수 있)
+            elif chosung == '' and jungsung == '' and jongsung != '':
+                result += char
+            
+        else:
+            result += char  # 한글이 아니면 그대로 추가
+    
+    return result
+
 
 def multiReplace(text: str, new: str, *olds: str):
     for oldPart in olds:
