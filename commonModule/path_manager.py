dcf7c6f7a062a2a8d5503fe02c75b7c498abe9f9
ityeri <yeongbin0909@naver.com>
2025-01-22 10:14:17 +0900

    필요없는 함수 주석처리

--- commonModule/path_manager.py
+++ commonModule/path_manager.py
@@ -12,13 +12,13 @@ def getDataFolder(path: str):
     return f'data/{path}'
 
 
-def getDataFile(path: str):
-    dataPathExist = False
-
-    if not( os.path.exists('data') and os.path.isdir('data') ):
-        os.mkdir('data')
-    
-    if not( os.path.exists(f'data/{path}') and os.path.isdir(f'data/{path}') ):
-        os.mkdir(f'data/{path}')
-
-    return f'data/{path}'
+# def getDataFile(path: str):
+#     dataPathExist = False
+#
+#     if not( os.path.exists('data') and os.path.isdir('data') ):
+#         os.mkdir('data')
+#
+#     if not( os.path.exists(f'data/{path}') and os.path.isdir(f'data/{path}') ):
+#         os.mkdir(f'data/{path}')
+#
+#     return f'data/{path}'
