0252e5da000a02cf3b1c0d9ad082577c7b120559
ityeri <yeongbin0909@naver.com>
2025-01-23 16:55:31 +0900

    PieBot 객체를 개별 파일로 분리

--- main.py
+++ main.py
@@ -1,60 +1,13 @@
-import os
-
 import nextcord.ext
 
-from _commands import _commands
 from _token import TOKEN
-from common_module.admin_manager import *
-
-
-class PieBot(commands.Bot):
-
-    def __init__(self, intents):
-        super().__init__(intents=intents)
-        self.adminManager: AdminManager = AdminManager()
-
-
-    def initFolder(self):
-
-        if not (os.path.exists("src/") and os.path.isdir("src/")):
-            os.mkdir("src/")
-        os.chdir("src/")
-
-        if not (os.path.exists("data/") and os.path.isdir("data/")):
-            os.mkdir("data/")
-
-
-    def loadCommands(self):
-        for commandPath in _commands:
-
-            # 혹시 익스텐션 로드 하는데 에러났니? _command.py 파일에 콤마 잘 넣었는지 봐봐
-            self.load_extension(f"commands.{commandPath}")
-
-            print(f"{commandPath} 등록 완료!")
-
-
-    async def on_ready(self):
-        print("\n================")
-        print(f'봇 "{self.user}" 에 연결했습니다!')
-        print("================\n")
-
-        print("명령어를 동기화 합니다...")
-        await self.sync_application_commands()
-        print("명령어 동기화 완료!\n")
-
-        self.adminManager.load(self)
-
-        print('\nTimings Reset\n')
-
+from pie_bot import PieBot
+from common_module.admin_manager import AdminManager
 
 if __name__ == "__main__":
-    intents = nextcord.Intents.all()
-    intents.message_content = True
+    pieBot: PieBot = PieBot()
 
-    bot: PieBot = PieBot(intents)
-    bot.initFolder()
-    bot.loadCommands()
-
+    pieBot.setAdminManager(AdminManager())
 
     print("\n봇을 키는중...")
-    bot.run(TOKEN)
+    pieBot.startBot(TOKEN)
