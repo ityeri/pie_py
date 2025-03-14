c1559ff4fe7c1419f1d9f5afa84a68111e9e0827
it <yeongbin0909@naver.com>
2025-03-14 23:11:12 +0900

    운영체제 확인 및, asyncio.WindowsSelectorEventLoopPolicy 설정부 추가

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import asyncio
+import platform
+
 from pie_bot import PieBot
 from common_module.admin_manager import AdminManager
 
@@ -5,6 +8,9 @@ try:
     from _token import TOKEN
 except ModuleNotFoundError:
     raise ModuleNotFoundError("봇의 토큰이 저장되는 _token.py 파일을 찾을수 없습니다.")
+
+if platform.system() == "Windows":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
 if __name__ == "__main__":
