4423258f1014f4d698aa0016053b280d943267f1
it <yeongbin0909@naver.com>
2025-03-14 23:09:42 +0900

    가독성 수정, StockTable 추가

--- commands/game/economy_tools/__init__.py
+++ commands/game/economy_tools/__init__.py
@@ -1,11 +1,20 @@
 from .stock import Stock
+
 from .user_bank_book import UserBankbook
 from .asset import Asset
+
 from .bank_book_manager import BankBookManager
+
+from .stocks_preset import StockTable, coins
 
 __all__ = [
     "Stock",
+
     "UserBankbook",
     "Asset",
-    "BankBookManager"
+
+    "BankBookManager",
+
+    "StockTable",
+    "coins",
 ]
