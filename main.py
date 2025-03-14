4406a2e5acff27b78cb274f032717c31c568d56d
it <yeongbin0909@naver.com>
2025-03-14 13:56:48 +0900

    _token.py를 불러올시 예외처리 추가

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from _token import TOKEN
 from pie_bot import PieBot
 from common_module.admin_manager import AdminManager
+
+try:
+    from _token import TOKEN
+except ModuleNotFoundError:
+    raise ModuleNotFoundError("봇의 토큰이 저장되는 _token.py 파일을 찾을수 없습니다.")
+
 
 if __name__ == "__main__":
     pieBot: PieBot = PieBot()
