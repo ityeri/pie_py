63ffe1428e4250b80f79569e3b15b0e73271d3a6
it <yeongbin0909@naver.com>
2025-03-15 03:51:53 +0900

    sell 메서드 추가

--- commands/game/economy_tools/asset.py
+++ commands/game/economy_tools/asset.py
@@ -8,4 +8,11 @@ class Asset:
     def buy(self, amount: float):
         self.amount += amount
 
+    def sell(self, amount: float) -> bool:
+        if amount <= self.amount:
+            self.amount -= amount
+            return True
+        else:
+            return False
+
     def get_krw(self) -> float: return self.amount * self.stock.get_krw()
