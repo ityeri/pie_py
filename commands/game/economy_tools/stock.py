468d4092bc08e7252a2f7212252dc5a94dc2a49e
it <yeongbin0909@naver.com>
2025-03-15 03:53:33 +0900

    이전 시간의 코인 가격도 가져올수 있도록 변경

--- commands/game/economy_tools/stock.py
+++ commands/game/economy_tools/stock.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     while True:
         print(ccxt.upbit().fetch_ticker("BTC/KRW")['last'])
 
+# 업비트 거레소 인스턴스
+exchange = ccxt.upbit()
+
 @abstractmethod
 class Stock:
     def __init__(self, symbol: str, name: str):
@@ -18,5 +21,13 @@ class Stock:
 
     def __hash__(self): return hash(self.symbol)
 
-    def get_krw(self, amount: float=1) -> float:
-        return ccxt.upbit().fetch_ticker(self.symbol + '/KRW')['last'] * amount
+    def get_krw(self, amount: float=1, time_ago_min: int=0) -> float:
+        if time_ago_min == 0:
+            return exchange.fetch_ticker(self.symbol + '/KRW')['last'] * amount
+        else:
+            current_time = exchange.milliseconds()
+            target_time = current_time - time_ago_min * 60 * 1000
+
+            # 해당 시간의 종가 가져옴
+            # timeframe 의 '1m' 은 1분 단위씩 데이터를 가져온다는 의미
+            return exchange.fetch_ohlcv(self.symbol + '/KRW', '1m', since=target_time)[0][4]
