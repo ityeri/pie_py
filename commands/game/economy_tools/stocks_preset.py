12e72b8aaa13bca61df35db814848b31a11bf559
it <yeongbin0909@naver.com>
2025-03-14 23:12:58 +0900

    StockTable 로 주식들을 따로 관리함

---
+++ commands/game/economy_tools/stocks_preset.py
@@ -0,0 +1,28 @@
+from .stock import Stock
+
+
+class StockTable:
+    def __init__(self, *stocks: Stock):
+        self.stocks: list[stocks] = list(stocks)
+
+    def get_stock_by_symbol(self, symbol):
+        return [stock for stock in self.stocks if stock.symbol == symbol][0]
+
+    def get_stock_by_name(self, symbol):
+        return [stock for stock in self.stocks if stock.name == symbol][0]
+
+
+    def get_symbols(self) -> list[str]:
+        return list(map(lambda x: x.symbol, self.stocks))
+
+    def get_names(self) -> list[str]:
+        return list(map(lambda x: x.name, self.stocks))
+
+
+coins = StockTable(
+    Stock("BTC", "비트코인"),
+    Stock("EHT", "이더리움"),
+    Stock("DOGE", "도지코인"),
+    Stock("TRUMP", "오피셜트럼프"),
+    Stock("USDT", "테더")
+)
