304b5cfee789d37886cbfe4df871be85d877c1f7
it <yeongbin0909@naver.com>
2025-03-16 01:32:44 +0900

    json 직렬화/역직렬화 기능 추가

--- commands/game/economy_tools/bank_book_manager.py
+++ commands/game/economy_tools/bank_book_manager.py
@@ -1,5 +1,9 @@
-from nextcord import User
+from typing import Any
 
+from nextcord import User
+from nextcord.ext.commands import Bot
+
+from .stocks_preset import StocksManager
 from .user_bank_book import UserBankbook
 
 
@@ -12,3 +16,17 @@ class BankBookManager:
         if user not in self.user_bank_books and auto_gen:
             self.user_bank_books[user] = UserBankbook(user, 1000_0000)
         return self.user_bank_books[user]
+
+
+
+    @classmethod
+    def from_json(cls, data: dict[int, dict], bot: Bot, stock_manager: StocksManager) -> 'BankBookManager':
+        bank_book_manager = BankBookManager()
+        for user_id, bank_book_data in data:
+            bank_book_manager.user_bank_books[bot.get_user(user_id)] = (
+                UserBankbook.from_json(bank_book_data, bot, stock_manager))
+
+        return bank_book_manager
+
+    def to_json(self) -> dict[int, dict]:
+        return {user.id: bank_book.to_json() for user, bank_book in self.user_bank_books.items()}
