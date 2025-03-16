a4e68e958bd1357ec9072392df4d2e26bdecf814
it <yeongbin0909@naver.com>
2025-03-16 13:20:00 +0900

    str_to_taste 재추가

--- commands/function/menu_tools/taste.py
+++ commands/function/menu_tools/taste.py
@@ -7,5 +7,11 @@ class Taste(Enum):
     RAW = (2, "삼")
 
     def __init__(self, taste_num: int, taste_str: str):
-        self.taste_num: int = taste_num
+        self._value_: int = taste_num
         self.taste_str: str = taste_str
+
+    @classmethod
+    def str_to_taste(cls, taste_str: str) -> "Taste":
+        for taste in cls:
+            if taste.taste_str == taste_str:
+                return taste
