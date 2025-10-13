9c2034dc966945288d392e35240f0cda8cd8f616
it <ityeri9@gmail.com>
2025-10-13 14:12:53 +0900

    Move on_ready function to pie_py.py

--- src/pie_py/__main__.py
+++ src/pie_py/__main__.py
@@ -9,22 +9,6 @@ from extensions import extensions
 from pie_py import bot, setup, log_handler, cli_runner
 
 
-@bot.event
-async def on_ready():
-    logging.info(f'"{bot.user}" 로 로그인 완료')
-
-    for ext in extensions:
-        logging.info(f'익스텐션 "{ext}" 등록중')
-        await bot.load_extension(ext)
-
-    logging.info("Done!")
-    logging.info("Timings Reset")
-
-    logging.info("명령어 동기화중...")
-    await bot.tree.sync()
-    logging.info("명령어 동기화 완료")
-
-
 if __name__ == "__main__":
     dotenv.load_dotenv(".env")
 
--- src/pie_py/pie_py.py
+++ src/pie_py/pie_py.py
@@ -20,6 +20,22 @@ cli_runner: CLIRunner = CLIRunner()
 bot: commands.Bot = commands.Bot(command_prefix="/", intents=intents)
 
 
+@bot.event
+async def on_ready():
+    logging.info(f'"{bot.user}" 로 로그인 완료')
+
+    for ext in extensions:
+        logging.info(f'익스텐션 "{ext}" 등록중')
+        await bot.load_extension(ext)
+
+    logging.info("Done!")
+    logging.info("Timings Reset")
+
+    logging.info("명령어 동기화중...")
+    await bot.tree.sync()
+    logging.info("명령어 동기화 완료")
+
+
 def setup():
     print("Setting up...")
     discord.utils.setup_logging()
