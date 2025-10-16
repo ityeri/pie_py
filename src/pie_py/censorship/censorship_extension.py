5420d2b39b9b716b06d42f9d0127a276bb67d97c
it <ityeri9@gmail.com>
2025-10-16 20:00:55 +0900

    Add censorship command group

--- src/pie_py/censorship/censorship_extension.py
+++ src/pie_py/censorship/censorship_extension.py
@@ -6,6 +6,9 @@ class CensorshipExtension(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot: commands.Bot = bot
 
+    @commands.hybrid_group(name='검열', description='검열 관리 명령어 모음')
+    def censorship(self, ctx: commands.Context): ...
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(CensorshipExtension(bot))
